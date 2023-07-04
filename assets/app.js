3e6e4d1a26a516143eed6ef30218e996756f6625
BLOS <blosguitarist@gmail.com>
2023-07-04 04:25:55 +0100

    trial not possible using current libraries

--- assets/app.js
+++ assets/app.js
@@ -2,13 +2,13 @@ const select = document.getElementById("audio-devices-input");
 
 // Create an AudioContext
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+audioContext.suspend();
 Tone.context.lookAhead = 0;
 Tone.context.updateInterval = 0.01;
 Tone.context.bufferSize = 128;
 
 // Audio Parameters
 const audioSource = new Tone.UserMedia();
-const monoSignal = new Tone.Mono();
 const destination = Tone.Destination;
 
 let mediaStream;
@@ -17,17 +17,19 @@ let sourceNode;
 // Handle device selection change
 select.addEventListener("change", async () => {
   const selectedDeviceId = select.value;
+  const selectedChannels = [1, 2]; // Modify this array to include the desired channel indices (starting from 1)
 
-  // Check if there is an active MediaStream and disconnect it
-  if (mediaStream && sourceNode) {
-    sourceNode.disconnect();
-    mediaStream.getTracks().forEach((track) => track.stop());
-  }
+  // Stop the previous audio
+  stopAudio();
 
   try {
-    // Create a MediaStream using the selected audio device
+    // Create a MediaStream using the selected audio device and channels
     mediaStream = await navigator.mediaDevices.getUserMedia({
-      audio: { deviceId: selectedDeviceId },
+      audio: {
+        deviceId: selectedDeviceId,
+        channelCount: selectedChannels.length,
+        channelSelection: selectedChannels,
+      },
     });
 
     // Create a MediaStreamAudioSourceNode
@@ -35,10 +37,6 @@ select.addEventListener("change", async () => {
 
     // Connect the source node to the audio context destination
     sourceNode.connect(audioContext.destination);
-
-    // Disconnect the audio source from the mono signal
-    audioSource.disconnect(monoSignal);
-    
   } catch (error) {
     console.error("Error accessing audio device:", error);
   }
@@ -54,14 +52,16 @@ navigator.mediaDevices
       .then((devices) => {
         // Filter audio input devices
         const audioInputDevices = devices.filter(
-          (device) => device.kind === "audioinput"
+          (device) =>
+            device.kind === "audioinput" &&
+            device.label.includes("Scarlett 18i20")
         );
 
         // Populate the select element with audio input devices
         audioInputDevices.forEach((device) => {
           const option = document.createElement("option");
           option.value = device.deviceId;
-          option.text = device.label || `Audio Input ${device.deviceId}`;
+          option.text = device.label;
           select.appendChild(option);
         });
       })
@@ -77,12 +77,17 @@ navigator.mediaDevices
 async function main() {
   // Start the audio context
   await Tone.start();
-
-  audioSource.open().then(() => {
-    console.log("Audio source opened");
-    audioSource.connect(monoSignal);
-    monoSignal.connect(destination);
-  });
 }
 
 main();
+
+function stopAudio() {
+  // Disconnect the source node and stop the media stream
+  if (sourceNode) {
+    sourceNode.disconnect();
+  }
+  if (mediaStream) {
+    mediaStream.getTracks().forEach((track) => track.stop());
+    mediaStream = null; // Reset the media stream variable
+  }
+}
